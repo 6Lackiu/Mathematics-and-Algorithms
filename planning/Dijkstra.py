9ce51f22abd854917f98cdce8129a6b272bbe073
Liu <819209862@qq.com>
2022-11-18 22:07:25 +0800

    update dijkstra

--- planning/Dijkstra.py
+++ planning/Dijkstra.py
@@ -88,9 +88,10 @@ def reconstruct_path(came_from, start, goal):
 
 
 
-came_from, cost_so_far = dijkstra_search(diagram4, (1, 4), (7, 8))
-draw_grid(diagram4, width=3, point_to=came_from, start=(1, 4), goal=(7, 8))
+start, goal = (1, 4), (8, 3)
+came_from, cost_so_far = dijkstra_search(diagram4, start, goal)
+draw_grid(diagram4, point_to=came_from, start=start, goal=goal)
 print()
-draw_grid(diagram4, width=3, number=cost_so_far, start=(1, 4), goal=(7, 8))
+draw_grid(diagram4, path=reconstruct_path(came_from, start=start, goal=goal))
 print()
-draw_grid(diagram4, width=3, path=reconstruct_path(came_from, start=(1, 4), goal=(7, 8)))
+draw_grid(diagram4, number=cost_so_far, start=start)
